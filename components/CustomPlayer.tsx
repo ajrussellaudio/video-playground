f222866d1caf8589a6bac0014317214872fe624e
Alan Russell <alan.russell@digirati.com>
2021-05-17 11:28:59 +0100

    Set dimensions on play

--- components/CustomPlayer.tsx
+++ components/CustomPlayer.tsx
@@ -4,13 +4,11 @@ import { CustomPlayerOverlay } from './CustomPlayerOverlay';
 
 const Container = styled.div`
   position: relative;
-  width: 640px;
-  height: 380px;
+  width: 100%;
+  height: 100%;
 `;
 
 const Video = styled.video`
-  position: absolute;
-  inset: 0;
   width: 100%;
   height: 100%;
 `;
@@ -25,13 +23,11 @@ export function CustomPlayer() {
   // https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
   const videoRef = useCallback((videoNode: HTMLVideoElement) => {
     if (videoNode !== null) {
-      const { videoHeight, videoWidth } = videoNode;
-      setDimensions({ width: videoWidth, height: videoHeight });
-
       videoNode.addEventListener('pause', function () {
         setState({ isPaused: true, time: this.currentTime });
       });
       videoNode.addEventListener('play', function () {
+        setDimensions({ width: this.videoWidth, height: this.videoHeight });
         setState({ isPaused: false });
       });
     }
@@ -40,7 +36,7 @@ export function CustomPlayer() {
   return (
     <Container>
       <Video ref={videoRef} controls>
-        <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4" />
+        <source src="//cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4" type="video/mp4" />
       </Video>
       {state.isPaused && <CustomPlayerOverlay time={state.time} {...dimensions} />}
     </Container>
